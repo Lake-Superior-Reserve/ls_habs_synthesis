e0e372b072ddb77d76e918b5ea56fde5d44c0773
Sam Blackburn <samblackburn@Sams-MacBook-Pro.local>
2024-09-04 09:09:52 -0500

    separate nwis and wqp

--- WQP Clean.R
+++ WQP Clean.R
@@ -7,111 +7,26 @@ library(readr)
 library(stringr)
 library(dplyr)
 library(tidyr)
+library(lubridate)
 library(leaflet)
 library(htmltools)
-library(lubridate)
-library(dataRetrieval)
-
-
-# hucs in area we're interested in
-huc_list <- c("04010102", "04010201", "04010202", "04010301", "04010302", "04020300") 
-
-#see what sites have continuous data
-nwis_pull <- readNWISdata(huc = huc_list, service = "dv", startDate = "2010-01-01")
-
-site_list <- readNWISdata(huc = huc_list, service = "site", startDate = "2010-01-01") %>%
-  filter(site_no %in% nwis_pull$site_no & site_tp_cd == "ST")
-
-leaflet() %>%
-  addProviderTiles('Esri.WorldStreetMap') %>%
-  addCircleMarkers(
-    data=site_list,
-    lng=~dec_long_va,
-    lat=~dec_lat_va,
-    label = ~htmlEscape(str_c(site_no, station_nm, sep = ", "))
-  )
-
-#sites that are more upstream than is really useful
-upstream_sites <- c("04015438", "465735092504901", "470535092570801", "471149092360901", "04021520", "04026450", "04026561", "04026740", "04001000")
-
-site_list <- filter(site_list, !(site_no %in% upstream_sites))
-
-leaflet() %>%
-  addProviderTiles('Esri.WorldStreetMap') %>%
-  addCircleMarkers(
-    data=site_list,
-    lng=~dec_long_va,
-    lat=~dec_lat_va,
-    label = ~htmlEscape(str_c(site_no, station_nm, sep = ", "))
-  )
-
-#site I had found that should have data:
-activesites <- c("04015330", "04024000", "464646092052900", "04024430", "04025500", "04026005", "04026157", "04026160", "465130091060701", "040263205", "463741090521301",
-                 "04026390", "463836090423701", "04027500", "040265935", "040265981", "04026740", "04026511", "04026561", "04026900", "04027000", "04027595", "04027655", 
-                 "463227090365601", "040276505")
-
-missingactive <- activesites[!(activesites %in% site_list$site_no) & !(activesites %in% upstream_sites)]
-
-for (site in missingactive) {
-  dv_check <- readNWISdata(sites = site, service = "dv")
-  if (nrow(dv_check) > 0) print(site)
-}
-#duluth returns a dv, but its bad data and won't return dvs if you give it a start date
-#data_464646092052900 <- readNWISdata(sites = "464646092052900", service = "dv", startDate = "2010-01-01")
-
-iv_sites <- c()
-for (site in missingactive) {
-  iv_check <- readNWISdata(sites = site, service = "iv")
-  if (nrow(iv_check) > 0) {
-    iv_sites <- append(iv_sites, site)
-  }
-}
-
-#sites with only instantaneous values available:
-missing_sitelist <- readNWISdata(site = iv_sites, service = "site")
-leaflet() %>%
-  addProviderTiles('Esri.WorldStreetMap') %>%
-  addCircleMarkers(
-    data=missing_sitelist,
-    lng=~dec_long_va,
-    lat=~dec_lat_va,
-    label = ~htmlEscape(str_c(site_no, station_nm, sep = ", "))
-  )
-
-# of those 9, 4 seem useful - c("463741090521301", "464646092052900", "463836090423701", "04027655")
-# in order of most usefulness Ashland breakwater, Duluth shipping canal, Kakagon river slough, bad river mouth
-# the others: 4 are bad river tributaries, 1 is upstream denomie creek
-# might want to pull IV data for sites that we have discrete observations for to match up gage data
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 ###########pulling WQP data 
 # huc_list <- c("04010102", "04010201", "04010202", "04010301", "04010302")
 # wqp_pull_trib <- TADA_BigDataRetrieval(huc = huc_list, startDate = "2010-01-01", endDate = "2023-12-31")
-# saveRDS(wqp_pull_trib, "Desktop/wqp_full_trib.rds")
-# write_csv(wqp_pull_trib, "Desktop/wqp_full_trib.csv")
+# saveRDS(wqp_pull_trib, "Documents/WQP/wqp_full_trib.rds")
+# write_csv(wqp_pull_trib, "Documents/WQP/wqp_full_trib.csv")
 # 
 # wqp_pull_ls <- TADA_BigDataRetrieval(huc = "04020300", startDate = "2010-01-01", endDate = "2023-12-31") 
-# saveRDS(wqp_pull_ls, "Desktop/wqp_full_ls.rds")
-# write_csv(wqp_pull_ls, "Desktop/wqp_full_ls.csv")
+# saveRDS(wqp_pull_ls, "Documents/WQP/wqp_full_ls.rds")
+# write_csv(wqp_pull_ls, "Documents/WQP/wqp_full_ls.csv")
 
-wqp_trib <- readRDS("Desktop/wqp_full_trib.rds") %>%
+wqp_trib <- readRDS("Documents/WQP/wqp_full_trib.rds") %>%
   TADA_AutoClean() %>%
   filter(TADA.LatitudeMeasure > 46) #drop sites missing lat/long
 
-wqp_ls <- readRDS("Desktop/wqp_full_ls.rds") %>%
+wqp_ls <- readRDS("Documents/WQP/wqp_full_ls.rds") %>%
   TADA_AutoClean() %>%
   filter(TADA.LatitudeMeasure < 47.3 & TADA.LongitudeMeasure < -90.0)
 
@@ -132,9 +47,9 @@ gc()
 
 # get synonym reference table to edit
 #synref <- TADA_GetSynonymRef(wqp2)
-# write_csv(synref, "Desktop/synref.csv", na = "")
+# write_csv(synref, "Documents/WQP/synref.csv", na = "")
 # read in once edits are done
-synref <- read_csv("Desktop/Synonym Reference Table.csv") 
+synref <- read_csv("Documents/WQP/Synonym Reference Table.csv") 
 
 wqp <- filter(wqp, TADA.CharacteristicName %in% unique(synref$TADA.CharacteristicName)) %>%
   TADA_HarmonizeSynonyms(ref = synref)
@@ -295,7 +210,7 @@ wqp <- select(wqp, -all_of(na_cols)) #%>%
 
 
 #write out base cleaned dataset
-write_rds(wqp, "Desktop/wqp.rds")
+write_rds(wqp, "Documents/WQP/wqp.rds")
 
 
 
