19a3c7f6d7fabde42839bb59678d70f6751f7b3f
Sam Blackburn <srblackburn@wisc.edu>
2024-10-22 15:32:51 -0500

    added nwis data

--- _targets.R
+++ _targets.R
@@ -1,6 +1,6 @@
 library(targets)
 
-read_packages <- c("tidyverse", "readxl", "sf", "EPATADA") #EPATADA needs to be installed from github
+read_packages <- c("tidyverse", "readxl", "sf", "EPATADA", "dataRetrieval") #EPATADA needs to be installed from github
 
 tar_option_set(packages = c(read_packages))
 
