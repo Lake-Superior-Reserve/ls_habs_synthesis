da3cc888d49a2e50cffe8bbe8b3529685fb83b33
Sam Blackburn <srblackburn@wisc.edu>
2025-01-21 15:57:12 -0600

    added nbdc solar radiation and 2024 data

--- R/read_targets/nbdc.R
+++ R/read_targets/nbdc.R
@@ -2,14 +2,14 @@ nbdc_targets <- list(
   
   # helper functions ----------------------------------------------
   
-  #' Pull NBDC data for a site
+  #' Pull NBDC historical meteorological data for a site
   #' 
-  #' Pulls data for a given site from 2010-2023 from NBDC. Also formats dates.
+  #' Pulls data for a given site from 2010-2024 from NBDC. Also formats dates.
   #'
   #' @param site NBDC site code
   #'
   #' @returns Data frame of data for site
-  tar_target(read_nbdc, function(site) {
+  tar_target(read_nbdc_stdmet, function(site) {
     nbdc_names <- c("YY", "MM", "DD", "hh", "mm", "wdir_degt", "wspd_ms", "gst_ms", "wvht_m", "dpd_s", "apd_s", "mwd_degt", "pres_hpa", "atemp_c", "wtemp_c", "dewp_c", "vis_mi", "tide_ft")
     historical = data.frame()
     for (year in 2010:2023) {
@@ -23,31 +23,110 @@ nbdc_targets <- list(
           select(-c(YY, MM, DD, hh, mm)) %>% 
           relocate(date, site)
         historical <- bind_rows(historical, nbdc)
-      },error=function(e){cat("ERROR: No data for", site, "in", year, "\n")})
+      },error=function(e){cat("ERROR: No stdmet data for", site, "in", year, "\n")})
+    }
+    # get 2024 by month, since historical isn't available yet
+    for (m in 1:12) {
+      nbdc_url <- str_c("https://www.ndbc.noaa.gov/view_text_file.php?filename=", site, m, "2024", ".txt.gz&dir=data/stdmet/", month(m, label = TRUE), "/")
+      tryCatch({
+        nbdc <- read_table(nbdc_url, skip = 2, col_names = nbdc_names) %>% 
+          mutate(date = str_c(YY, "-", MM, "-", DD, " ", hh, ":", mm),
+                 date = ymd_hm(date),
+                 across(-c(date), ~if_else(.x == 99 | .x == 999, NA, .x)),
+                 site = site) %>% 
+          select(-c(YY, MM, DD, hh, mm)) %>% 
+          relocate(date, site)
+        historical <- bind_rows(historical, nbdc)
+      },error=function(e){cat("ERROR: No stdmet data for", site, "in", month(m, label = TRUE), "2024", "\n")})
     }
     return(historical)
   }),
   
+  #' Pull NBDC historical solar radiation data for a site
+  #' 
+  #' Pulls data for a given site from 2010-2024 from NBDC. Also formats dates.
+  #'
+  #' @param site NBDC site code
+  #'
+  #' @returns Data frame of data for site
+  tar_target(read_nbdc_srad, function(site) {
+    nbdc_names <- c("YY", "MM", "DD", "hh", "mm", "srad_wm2", "swrad_wm2", "lwrad_wm2")
+    historical = data.frame()
+    for (year in 2010:2023) {
+      nbdc_url <- str_c("https://www.ndbc.noaa.gov/view_text_file.php?filename=", site, "r", year, ".txt.gz&dir=data/historical/srad/")
+      tryCatch({
+        nbdc <- read_table(nbdc_url, skip = 2, col_names = nbdc_names) %>% 
+          mutate(date = str_c(YY, "-", MM, "-", DD, " ", hh, ":", mm),
+                 date = ymd_hm(date),
+                 across(-c(date), ~if_else(.x == 9999, NA, .x)),
+                 site = site) %>% 
+          select(-c(YY, MM, DD, hh, mm)) %>% 
+          relocate(date, site)
+        historical <- bind_rows(historical, nbdc)
+      },error=function(e){cat("ERROR: No srad data for", site, "in", year, "\n")})
+    }
+    # get 2024 by month, since historical isn't available yet
+    for (m in 1:12) {
+      nbdc_url <- str_c("https://www.ndbc.noaa.gov/view_text_file.php?filename=", site, m, "2024", ".txt.gz&dir=data/srad/", month(m, label = TRUE), "/")
+      tryCatch({
+        nbdc <- read_table(nbdc_url, skip = 2, col_names = nbdc_names) %>% 
+          mutate(date = str_c(YY, "-", MM, "-", DD, " ", hh, ":", mm),
+                 date = ymd_hm(date),
+                 across(-c(date), ~if_else(.x == 9999, NA, .x)),
+                 site = site) %>% 
+          select(-c(YY, MM, DD, hh, mm)) %>% 
+          relocate(date, site)
+        historical <- bind_rows(historical, nbdc)
+      },error=function(e){cat("ERROR: No srad data for", site, "in", month(m, label = TRUE), "2024", "\n")})
+    }
+    return(historical)
+  }),
   
   # get data ---------------------------------------------------------
   
-  #' Pull NBDC data for sites
+  #' Pull NBDC meteorological historical data for sites
   #' 
-  #' Pulls data for Western Lake Superior buoys from 2010-2023 from NBDC. Then, bind data together
+  #' Pulls data for Western Lake Superior buoys from 2010-2024 from NBDC. Then, bind data together
   #'
   #' @param site NBDC site codes
   #'
   #' @returns Data frame of data for all sites
-  tar_target(nbdc_dulm5, read_nbdc("dulm5")),
-  tar_target(nbdc_45027, read_nbdc("45027")),
-  tar_target(nbdc_45028, read_nbdc("45028")),
-  tar_target(nbdc_45219, read_nbdc("45219")),
-  tar_target(nbdc_pngw3, read_nbdc("pngw3")),
-  tar_target(nbdc_slvm5, read_nbdc("slvm5")),
-  tar_target(nbdc_disw3, read_nbdc("disw3")),
-  tar_target(nbdc_sxhw3, read_nbdc("sxhw3")),
-  tar_target(nbdc, bind_rows(nbdc_dulm5, nbdc_45027, nbdc_45028, nbdc_45219, nbdc_pngw3, nbdc_slvm5, nbdc_disw3, nbdc_sxhw3)),
+  tar_target(nbdc_dulm5_stdmet, read_nbdc_stdmet("dulm5")),
+  tar_target(nbdc_45027_stdmet, read_nbdc_stdmet("45027")),
+  tar_target(nbdc_45028_stdmet, read_nbdc_stdmet("45028")),
+  tar_target(nbdc_45219_stdmet, read_nbdc_stdmet("45219")),
+  tar_target(nbdc_pngw3_stdmet, read_nbdc_stdmet("pngw3")),
+  tar_target(nbdc_slvm5_stdmet, read_nbdc_stdmet("slvm5")),
+  tar_target(nbdc_disw3_stdmet, read_nbdc_stdmet("disw3")),
+  tar_target(nbdc_sxhw3_stdmet, read_nbdc_stdmet("sxhw3")),
+  tar_target(nbdc_stdmet, bind_rows(nbdc_dulm5_stdmet, nbdc_45027_stdmet, nbdc_45028_stdmet, nbdc_45219_stdmet,
+                             nbdc_pngw3_stdmet, nbdc_slvm5_stdmet, nbdc_disw3_stdmet, nbdc_sxhw3_stdmet)),
   
+  #' Pull NBDC solar radiation historical data for sites
+  #' 
+  #' Pulls data for Western Lake Superior buoys from 2010-2024 from NBDC. Then, bind data together
+  #'
+  #' @param site NBDC site codes
+  #'
+  #' @returns Data frame of data for all sites
+  tar_target(nbdc_dulm5_srad, read_nbdc_srad("dulm5")),
+  tar_target(nbdc_45027_srad, read_nbdc_srad("45027")),
+  tar_target(nbdc_45028_srad, read_nbdc_srad("45028")),
+  tar_target(nbdc_45219_srad, read_nbdc_srad("45219")),
+  tar_target(nbdc_pngw3_srad, read_nbdc_srad("pngw3")),
+  tar_target(nbdc_slvm5_srad, read_nbdc_srad("slvm5")),
+  tar_target(nbdc_disw3_srad, read_nbdc_srad("disw3")),
+  tar_target(nbdc_sxhw3_srad, read_nbdc_srad("sxhw3")),
+  tar_target(nbdc_srad, bind_rows(nbdc_dulm5_srad, nbdc_45027_srad, nbdc_45028_srad, nbdc_45219_srad,
+                             nbdc_pngw3_srad, nbdc_slvm5_srad, nbdc_disw3_srad, nbdc_sxhw3_srad)),
+  
+  #' Join solar radiation and meteorological data
+  #' 
+  #' Combine the two big files by site and date
+  #'
+  #' @returns Data frame of all NBDC data for all sites
+  tar_target(nbdc, full_join(nbdc_stdmet, nbdc_srad) %>% 
+               arrange(date, site)),
   
   # make daily version of data -------------------------------------------
   
